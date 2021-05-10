cd17fac3a52a27f64503c9c31252a70c8d3c72c4
Tausif Mahmud <tmahmud44@gmail.com>
2021-05-10 07:17:21 +0600

    drawmethods applied test 1 passed

--- magicPaint.py
+++ magicPaint.py
@@ -4,54 +4,31 @@ import numpy as np
 #Capture
 cap = cv.VideoCapture(0)
 
-#gird method
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv.cvtColor( imgArray[x][y], cv.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv.cvtColor(imgArray[x], cv.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+myColors = [[0,100,53,56,255,255], #orange [0] =name 1:4 => h_min-s_min-v_min 4:7 => h_max-s_max-v_max [7] = pointer color
+            [39,119,0,179,255,255],    
+            [24,48,0,107,113,95]]
 
+ColorCode  = [[0,0,255],[255,165,0],[0,128,0]] #orange blue green
 
-myColors = [["orange",0,100,53,56,255,255,(0,0,255)], #orange [0] =name 1:4 => h_min-s_min-v_min 4:7 => h_max-s_max-v_max [7] = pointer color
-            ["blue",39,119,0,179,255,255,(255,165,0)],    
-            ["green",24,48,0,107,113,95,(0,128,0)]] 
+myPoints = []  #x,y,colorindex
 
 
 #find colors method
-def findColors(img,myColors):
+def findColors(img,myColors,pointColor):
     imgHSV = cv.cvtColor(img,cv.COLOR_BGR2HSV)
-    for c in myColors:
-        lower = np.array(c[1:4])  #lower range values array
-        upper = np.array(c[4:7])  #Upper range values array
+    counter = 0
+    newPoints = []
+    for color in myColors:
+        lower = np.array(color[0:3])  #lower range values array
+        upper = np.array(color[3:6])  #Upper range values array
         mask = cv.inRange(imgHSV,lower,upper) #mask photo b&ws
-        # cv.imshow((c[0]),mask)
         x,y = getContours(mask)
-        cv.circle(imgResult,(x,y),8,c[7],cv.FILLED)
+        cv.circle(imgResult,(x,y),8,pointColor[counter],cv.FILLED)
+        if x!=0 and y != 0:
+            newPoints.append([x,y,counter])
+        counter +=1
+    print(newPoints)
+    return newPoints
 
 #Contoures method
 
@@ -68,10 +45,23 @@ def getContours(img):
             x, y, w, h = cv.boundingRect(aprox) #it will help to create a box aroud the shape for detect the shape it has 4 values
     return x+w//2 , y
 
+
+def drawOnCanvas(recntPoints,pointColor):
+    for point in recntPoints:
+        cv.circle(imgResult,(point[0],point[1]),8,pointColor[point[2]],cv.FILLED)
+
+
+
 while True:
     isTrue, img = cap.read()
     imgResult = img.copy()
-    findColors(img,myColors)
+    newPoints = findColors(img,myColors,ColorCode)
+    if len(newPoints) !=0:
+        for newP in newPoints:
+            myPoints.append(newP)
+    if len(myPoints)!=0:
+        drawOnCanvas(myPoints,ColorCode)
+    
     cv.imshow("Display", imgResult)
     if cv.waitKey(1) & 0xFF == ord('f'):
         break
